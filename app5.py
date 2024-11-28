d14a271602ee163420ca558c3dd4eb81125967d8
misshivalal <mis@shivalal.in>
2024-11-28 17:59:45 +0530

    Update app5.py

--- app5.py
+++ app5.py
@@ -62,7 +62,7 @@ def login():
 def generate_pdf(certificate, footer_text="Default Admin Text"):
     try:
         # Paths
-        template_path = 'https://raw.githubusercontent.com/misshivalal/certificate/main/image'
+        template_path = https://raw.githubusercontent.com/misshivalal/certificate/main/image
 
         # Step 1: Create PDF overlay with student information
         packet = BytesIO()
